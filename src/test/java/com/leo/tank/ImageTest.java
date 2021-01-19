7bdc233b0c2b494ad316b5c60f9af80bae116ed3
Leo <582717189@qq.com>
2021-01-19 15:40:01 +0800

    add maven

--- src/test/java/com/leo/tank/ImageTest.java
+++ src/test/java/com/leo/tank/ImageTest.java
@@ -1,13 +1,14 @@
 package com.leo.tank;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.Assert.assertNotNull;
+
 
 /**
  * @author Leo
